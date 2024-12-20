e2bf9f439eefd00432cb4eda3b9be63783c5a05c
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-12-20 19:17:47 +0900

    [add]B325

---
+++ abc/B/B325/README.md
@@ -0,0 +1,11 @@
+# リンク
+https://www.notion.so/B-325-meeting-cffeeb02b38149f8aea5e1092f28ff34?pvs=4
+
+# 入力
+3
+5 0
+3 3
+2 18
+
+# 出力
+8
---
+++ abc/B/B325/atcoder.cpp
@@ -0,0 +1,11 @@
+// g++ -o atcoder atcoder.cpp
+// ./atcoder
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {}
