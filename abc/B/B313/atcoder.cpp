ad8b8beeb3248f6e3ac04fa3a3dd2fec4d0ebb9a
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-12-20 19:06:45 +0900

    [add]B313

---
+++ abc/B/B313/atcoder.cpp
@@ -0,0 +1,37 @@
+// g++ -o atcoder atcoder.cpp
+// ./atcoder
+
+// #include <algorithm>
+// #include <cmath>
+// #include <iostream>
+// #include <vector>
+
+// #define rep(i, n) for (int i = 0; i < (n); ++i)
+// using namespace std;
+// const double PI = acos(-1);
+// template <class T>
+// void chmax(T& a, T b) {
+//   if (a < b) {
+//     a = b;
+//   }
+// }
+
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// int main() {
+//   int n, m;
+//   cin >> n >> m;
+//   set<int> st;
+//   for (int i = 1; i <= n; i++) st.insert(i);
+//   rep(i, m) {
+//     int a, b;
+//     cin >> a >> b;
+//     st.erase(b);
+//   }
+//   if (st.size() >= 2)
+//     cout << -1 << endl;
+//   else
+//     cout << *st.begin() << endl;
+// }
