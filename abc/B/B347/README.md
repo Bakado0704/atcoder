f4391e13992ac777f41bc4aac0dea2c474ab6c22
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-12-20 19:43:03 +0900

    [add]B347

---
+++ abc/B/B347/README.md
@@ -0,0 +1,8 @@
+# リンク
+https://www.notion.so/B-347-1eab2e47c7bf4e6094e0ca3dbac83a6c?pvs=4
+
+# 入力
+yay
+
+# 出力
+5
---
+++ abc/B/B347/atcoder.cpp
@@ -0,0 +1,19 @@
+// g++ -o atcoder atcoder.cpp
+// ./atcoder
+
+// #include <algorithm>
+// #include <cmath>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main() {
+//   string s;
+//   cin >> s;
+//   set<string> st;
+//   for (int i = 0; i < s.size(); i++) {
+//     for (int j = 1; i + j <= s.size(); j++) {
+//       st.insert(s.substr(i, j));
+//     }
+//   }
+//   cout << st.size() << endl;
+// }
