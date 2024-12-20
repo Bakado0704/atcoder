c898c7f01522981bab294e8b5ef01229b8f60b88
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-12-20 19:21:45 +0900

    [add]B329

---
+++ abc/B/B329/README.md
@@ -0,0 +1,9 @@
+# リンク
+https://www.notion.so/B-329-0ae3688bf8a245acb3a5608b9f7f9840?pvs=4
+
+# 入力
+5
+2 1 3 3 2
+
+# 出力
+2
---
+++ abc/B/B329/atcoder.cpp
@@ -0,0 +1,35 @@
+// g++ -o atcoder atcoder.cpp
+// ./atcoder
+
+// #include <algorithm>
+// #include <cmath>
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < (n); ++i)
+// const double PI = acos(-1);
+// template <class T>
+// void chmax(T& a, T b) {
+//   if (a < b) {
+//     a = b;
+//   }
+// }
+
+// const long long INF = 1LL << 60;
+
+// int main() {
+//   int N;
+//   cin >> N;
+
+//   set<int> sr;
+//   vector<int> A(N);
+
+//   rep(i, N) cin >> A[i];
+//   rep(i, N) sr.insert(A[i]);
+
+//   auto itr = sr.rbegin();
+//   itr++;
+
+//   cout << *itr << endl;
+// }
