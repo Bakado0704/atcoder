e00fe0d110370ef604876e1069fc52a934980358
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-12-20 19:27:27 +0900

    [add]B334

---
+++ abc/B/B334/atcoder.cpp
@@ -0,0 +1,24 @@
+// g++ -o atcoder atcoder.cpp
+// ./atcoder
+
+// #include <algorithm>
+// #include <cmath>
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < (n); ++i)
+
+// int main() {
+//   long long A, M, L, R;
+//   cin >> A >> M >> L >> R;
+//   long long diff = R - L;
+
+//   long long count = 0;
+
+//   for (long long i = R; L <= i; i--) {
+//     if (i % M == 0) count++;
+//   }
+
+//   cout << count << endl;
+// }
