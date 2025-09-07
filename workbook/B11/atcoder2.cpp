efed0183d1ae260b39348b58eab86ecc72db2622
Bakado0704 <kado_hiroki@yahoo.co.jp>
2025-09-07 21:52:33 +0900

    B11

--- workbook/B11/atcoder2.cpp
+++ workbook/B11/atcoder2.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 -o atcoder atcoder.cpp
 // ./atcoder
-// 2025/9/7
+// 2025/8/5
 
 #include <algorithm>
 #include <cmath>
@@ -11,28 +11,31 @@ using namespace std;
 
 int search(int x, vector<int> A) {
   vector<int>::iterator pos = lower_bound(A.begin(), A.end(), x);
-  int dist = distance(A.begin(), pos);
-  return dist;
+  return distance(A.begin(), pos);
 }
 
 int main() {
   int N;
   cin >> N;
+
   vector<int> A(N);
 
   for (int i = 0; i < N; i++) {
     cin >> A[i];
   }
+
   sort(A.begin(), A.end());
 
   int Q;
   cin >> Q;
 
+  vector<int> X(Q);
   for (int i = 0; i < Q; i++) {
-    int X;
-    cin >> X;
+    cin >> X[i];
+  }
 
-    int result = search(X, A);
-    cout << result << endl;
+  for (int i = 0; i < Q; i++) {
+    int ans = search(X[i], A);
+    cout << ans << endl;
   }
 }
