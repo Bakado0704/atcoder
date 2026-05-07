d438e1c1b5748a0c6e4e9d9f5fc4455ad305b94d
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-05-07 21:39:08 +0900

    A09

--- workbook/A09/atcoder3.cpp
+++ workbook/A09/atcoder3.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 -o atcoder atcoder.cpp
 // ./atcoder
-// 2026/5/7
+// 2025/8/4
 
 #include <algorithm>
 #include <cmath>
@@ -13,7 +13,7 @@ int main() {
   int H, W, N;
   cin >> H >> W >> N;
 
-  vector<vector<int>> Increase(H, vector<int>(W, 0));
+  vector<vector<int>> Increase(H + 1, vector<int>(W + 1, 0));
   vector<int> A(N);
   vector<int> B(N);
   vector<int> C(N);
@@ -21,29 +21,38 @@ int main() {
 
   for (int i = 0; i < N; i++) {
     cin >> A[i] >> B[i] >> C[i] >> D[i];
+  }
+
+  for (int i = 0; i < N; i++) {
     Increase[A[i] - 1][B[i] - 1]++;
     Increase[C[i]][D[i]]++;
     Increase[A[i] - 1][D[i]]--;
     Increase[C[i]][B[i] - 1]--;
   }
 
+  vector<vector<int>> Snow(H, vector<int>(W, 0));
+
   // 横に加算
   for (int i = 0; i < H; i++) {
-    for (int j = 1; j < W; j++) {
-      Increase[i][j] = Increase[i][j - 1] + Increase[i][j];
+    for (int j = 0; j < W; j++) {
+      if (j == 0) {
+        Snow[i][j] = Increase[i][j];
+      } else {
+        Snow[i][j] = Snow[i][j - 1] + Increase[i][j];
+      }
     }
   }
 
-  // 縦に加算
+  // 　縦に加算
   for (int i = 1; i < H; i++) {
     for (int j = 0; j < W; j++) {
-      Increase[i][j] = Increase[i - 1][j] + Increase[i][j];
+      Snow[i][j] = Snow[i - 1][j] + Snow[i][j];
     }
   }
 
   for (int i = 0; i < H; i++) {
     for (int j = 0; j < W; j++) {
-      cout << Increase[i][j];
+      cout << Snow[i][j] << " ";
     }
     cout << endl;
   }
