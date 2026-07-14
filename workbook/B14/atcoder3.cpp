b4702a327daa08b648dba6f93f35f583c71d3ab2
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-07-15 08:57:13 +0900

    B14

--- workbook/B14/atcoder3.cpp
+++ workbook/B14/atcoder3.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 -o atcoder atcoder.cpp
 // ./atcoder
-// 2026/7/15
+// 2026/7/14
 
 #include <algorithm>
 #include <cmath>
@@ -15,7 +15,9 @@ int main() {
   vector<int> A(N);
   vector<int> P(N);
   vector<int> Q(N);
+
   for (int i = 0; i < N; i++) cin >> A[i];
+
   cin >> K;
 
   int halfNum = N / 2;
@@ -33,27 +35,31 @@ int main() {
   vector<int> QQ((1 << remainingNum));
 
   for (int bit = 0; bit < (1 << halfNum); bit++) {
+    int sum = 0;
     for (int i = 0; i < halfNum; i++) {
       if (bit & (1 << i)) {
-        PP[bit] += P[i];
+        sum += P[i];
       }
     }
+    PP[bit] = sum;
   }
 
   for (int bit = 0; bit < (1 << remainingNum); bit++) {
+    int sum = 0;
     for (int i = 0; i < remainingNum; i++) {
       if (bit & (1 << i)) {
-        QQ[bit] += Q[i];
+        sum += Q[i];
       }
     }
+    QQ[bit] = sum;
   }
 
-  sort(Q.begin(), Q.end());
+  sort(QQ.begin(), QQ.end());
 
   for (int bit = 0; bit < (1 << halfNum); bit++) {
     int target = K - PP[bit];
     vector<int>::iterator it = lower_bound(QQ.begin(), QQ.end(), target);
-    if (it != QQ.end() && *it == target) {
+    if (it != Q.end() && *it == target) {
       cout << "Yes" << endl;
       return 0;
     }
