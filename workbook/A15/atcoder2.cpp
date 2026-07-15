95e50042c3a56c2ef2434096ffdf10ffcf42f42f
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-07-16 08:45:22 +0900

    A15

--- workbook/A15/atcoder2.cpp
+++ workbook/A15/atcoder2.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 -o atcoder atcoder.cpp
 // ./atcoder
-// 2026/7/16
+// 2026/7/15
 
 #include <algorithm>
 #include <cmath>
@@ -13,22 +13,22 @@ int main() {
   int N;
   cin >> N;
   vector<int> A(N);
+  vector<int> B(N);
   vector<int> AA(N);
-
   for (int i = 0; i < N; i++) cin >> A[i];
-  for (int i = 0; i < N; i++) {
-    AA[i] = A[i];
-  }
-
+  for (int i = 0; i < N; i++) AA[i] = A[i];
   sort(AA.begin(), AA.end());
   AA.erase(unique(AA.begin(), AA.end()), AA.end());
 
   for (int i = 0; i < N; i++) {
     vector<int>::iterator it = lower_bound(AA.begin(), AA.end(), A[i]);
-    int index = it - AA.begin() + 1;
-    cout << index << " ";
+    int index = it - AA.begin();
+    B[i] = index + 1;
+  }
+
+  for (int i = 0; i < N; i++) {
+    cout << B[i] << " ";
   }
 
   cout << endl;
-  return 0;
 }
