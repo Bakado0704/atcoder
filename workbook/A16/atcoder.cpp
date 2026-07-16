2acdfaa88c01b16ca16bd496a0306193a6a48c69
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-07-16 18:20:34 +0900

    A16

---
+++ workbook/A16/atcoder.cpp
@@ -0,0 +1,30 @@
+// g++ -std=c++11 -o atcoder atcoder.cpp
+// ./atcoder
+// 2026/7/16
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N - 1);
+  vector<int> B(N - 2);
+
+  for (int i = 0; i < N - 1; i++) cin >> A[i];
+  for (int i = 0; i < N - 2; i++) cin >> B[i];
+
+  int DP[100];
+
+  DP[1] = 0;
+  DP[2] = A[0];
+  for (int i = 3; i <= N; i++) {
+    DP[i] = min(DP[i - 1] + A[i - 2], DP[i - 2] + B[i - 3]);
+  }
+
+  cout << DP[N] << endl;
+}
