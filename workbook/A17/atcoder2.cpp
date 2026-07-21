795c19bc661edd359701c9fe6a5d4aa4d9ec20b2
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-07-22 08:43:45 +0900

    A17

--- workbook/A17/atcoder2.cpp
+++ workbook/A17/atcoder2.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 -o atcoder atcoder.cpp
 // ./atcoder
-// 2026/7/22
+// 2026/7/21
 
 #include <algorithm>
 #include <cmath>
@@ -18,6 +18,7 @@ int main() {
   for (int i = 0; i < N - 1; i++) cin >> A[i];
   for (int i = 0; i < N - 2; i++) cin >> B[i];
   int DP[100];
+
   DP[1] = 0;
   DP[2] = A[0];
   for (int i = 3; i <= N; i++) {
@@ -25,10 +26,11 @@ int main() {
   }
 
   int place = N;
-  for (int i = 1; i <= N; i++) {
+  while (true) {
     Answer.push_back(place);
     if (place == 1) break;
-    if (DP[place] == DP[place - 1] + A[place - 2])
+
+    if (DP[place] == (DP[place - 1] + A[place - 2]))
       place--;
     else
       place -= 2;
