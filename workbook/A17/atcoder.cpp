58e2fd40fb2bb768d6664e8d8ad3e27528bb9830
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-07-21 17:30:51 +0900

    A17

---
+++ workbook/A17/atcoder.cpp
@@ -0,0 +1,48 @@
+// g++ -std=c++11 -o atcoder atcoder.cpp
+// ./atcoder
+// 2026/7/21
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N - 1);
+  vector<int> B(N - 2);
+  vector<int> Answer;
+  for (int i = 0; i < N - 1; i++) cin >> A[i];
+  for (int i = 0; i < N - 2; i++) cin >> B[i];
+  int DP[100];
+
+  DP[1] = 0;
+  DP[2] = A[0];
+  for (int i = 3; i <= N; i++) {
+    DP[i] = min(DP[i - 1] + A[i - 2], DP[i - 2] + B[i - 3]);
+  }
+
+  int place = N;
+  while (true) {
+    Answer.push_back(place);
+    if (place == 1) break;
+
+    if (DP[place] == (DP[place - 1] + A[place - 2]))
+      place--;
+    else
+      place -= 2;
+  }
+
+  sort(Answer.begin(), Answer.end());
+
+  cout << Answer.size() << endl;
+
+  for (int i = 0; i < Answer.size(); i++) {
+    cout << Answer[i] << " ";
+  }
+
+  cout << endl;
+}
