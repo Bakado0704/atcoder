23bc9e36224652ae2d939d3495d50c09e45eccf3
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-08-17 07:43:00 +0900

    B22

--- workbook/B22/atcoder3.cpp
+++ workbook/B22/atcoder3.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 -o atcoder atcoder.cpp
 // ./atcoder
-// 2026/8/17
+// 2026/8/12
 
 #include <algorithm>
 #include <cmath>
@@ -12,20 +12,17 @@ using namespace std;
 int main() {
   int N;
   cin >> N;
-  vector<int> A(N);
-  vector<int> B(N - 1);
-
+  vector<int> A(N, 0);
+  vector<int> B(N - 1, 0);
   for (int i = 1; i < N; i++) cin >> A[i];
   for (int i = 1; i < N - 1; i++) cin >> B[i];
-
-  vector<int> DP(N);
+  vector<int> DP(N + 1, 1000);
   DP[1] = 0;
-
   for (int i = 2; i <= N; i++) {
     if (i >= 3) {
-      DP[i] = DP[i - 1] + A[i - 1];
+      DP[i] = min({DP[i - 2] + B[i - 2], DP[i], DP[i - 1] + A[i - 1]});
     } else {
-      DP[i] = min(DP[i - 1] + A[i - 1], DP[i - 2] + B[i - 2]);
+      DP[i] = min(DP[i], DP[i - 1] + A[i - 1]);
     }
   }
 
