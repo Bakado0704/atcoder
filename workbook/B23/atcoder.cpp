b8b9fb19f587f8fc841f632f594d3214a7ac2205
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-08-21 12:51:25 +0900

    B23

---
+++ workbook/B23/atcoder.cpp
@@ -0,0 +1,15 @@
+// g++ -std=c++11 -o atcoder atcoder.cpp
+// ./atcoder
+// 2026/8/19
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M, A[109][19];
+int DP[109][1024];
+
+int main() {}
