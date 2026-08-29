6bfcd3e0a13aa942faad16ab46b5624e4136c1ff
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-08-29 22:23:20 +0900

    473-A

---
+++ abc/473/A/atcoder.cpp
@@ -0,0 +1,25 @@
+// g++ -std=c++11 -o atcoder atcoder.cpp
+// ./atcoder
+// 2026/8/10
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) cin >> A[i];
+  int ans = 0;
+  int begin = N / 2;
+  for (int i = begin; i < N; i++) {
+    ans += A[i];
+  }
+
+  cout << ans << endl;
+  return 0;
+}
