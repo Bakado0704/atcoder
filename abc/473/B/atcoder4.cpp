5b3da5b969a44074b338a9555b4614e89ae8fe30
Bakado0704 <kado_hiroki@yahoo.co.jp>
2026-09-08 08:40:31 +0900

    473-B

--- abc/473/B/atcoder4.cpp
+++ abc/473/B/atcoder4.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 -o atcoder atcoder.cpp
 // ./atcoder
-// 2026/9/8
+// 2026/9/7
 
 #include <algorithm>
 #include <cmath>
@@ -14,24 +14,28 @@ int main() {
   cin >> N;
   vector<int> A(N);
   for (int i = 0; i < N; i++) cin >> A[i];
+
   int ans = 0;
-  while(!A.empty()) {
+
+  while (!A.empty()) {
     bool found = false;
-    int a = A.back();
+    int target = A.back();
     A.pop_back();
+
     for (int i = 0; i < A.size(); i++) {
-      if (A[i] == a) {
-        A.erase(A.begin() + i);
+      if (A[i] == target) {
         found = true;
+        A.erase(A.begin() + i);
         break;
       }
     }
 
     if (!found) {
-      ans += a;
+      ans += target;
     }
   }
 
   cout << ans << endl;
+
   return 0;
 }
